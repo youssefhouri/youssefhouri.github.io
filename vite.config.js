7576a050452ca044a242decd0035b8eb17ef9ace
youssefhouri <youssef.houri@outlook.com>
2023-04-23 20:29:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 //https://youssefhouri.github.io/
 export default defineConfig({
+  base: "/youssefhouri.github.io/",
   plugins: [react()],
 })
